2ae2e3f9ffcd0fc15342dbcbadde07a908ec7bcb
Giovanni Monti <smartdavide@gmail.com>
2022-08-21 23:09:55 +0200

    added moment & moment timezone libraries added italian local time in top left

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.getElementById("github_profile").onclick = function(){
     window.open("https://github.com/GiovanniMonti", '_blank').focus();
 }
+
+function startTime(){
+    const clock = new Date();
+    let h = clock.getHours();
+    let  m = clock.getMinutes();
+    let s = clock.getSeconds();
+
+    let time = h + ":" + m + ":" + s;
+
+    document.getElementById("clock").textContent = time;
+    document.getElementById("clock").innerText = time;
+
+    setTimeout(startTime,1000);
+} //startTime();
+
+function startTime_n(){
+    let time = moment().tz("Europe/Rome").format("h:m:s");
+
+    document.getElementById("clock").textContent = time;
+    document.getElementById("clock").innerText = time;
+
+    setTimeout(startTime,1000);
+}startTime_n()
