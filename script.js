42d166bfb830a7d09d167b441cdf18c6cf734355
Giovanni Monti <smartdavide@gmail.com>
2022-08-22 10:56:51 +0200

    attempt at using gitattributes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById("github_profile").onclick = function(){
     window.open("https://github.com/GiovanniMonti", '_blank').focus();
 }
-
+/*
+this is no longer used, now using moment library for timezoned time.
 function startTime(){
     const clock = new Date();
     let h = clock.getHours();
@@ -16,6 +17,7 @@ function startTime(){
     setTimeout(startTime,1000);
 } //startTime();
 
+*/
 function startTime_n(){
     let time = moment().tz("Europe/Rome").format("h:m:s");
 
@@ -23,4 +25,4 @@ function startTime_n(){
     document.getElementById("clock").innerText = time;
 
     setTimeout(startTime,1000);
-}startTime_n()
+}startTime_n();
