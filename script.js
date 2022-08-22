f8ef5caf2ffd88cc6e907e57e6f8104417bd2544
Giovanni Monti <smartdavide@gmail.com>
2022-08-22 12:03:48 +0200

    removed old code

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 document.getElementById("github_profile").onclick = function(){
     window.open("https://github.com/GiovanniMonti", '_blank').focus();
 }
-/*
-this is no longer used, now using moment library for timezoned time.
-function startTime(){
-    const clock = new Date();
-    let h = clock.getHours();
-    let  m = clock.getMinutes();
-    let s = clock.getSeconds();
 
-    let time = h + ":" + m + ":" + s;
-
-    document.getElementById("clock").textContent = time;
-    document.getElementById("clock").innerText = time;
-
-    setTimeout(startTime,1000);
-} //startTime();
-
-*/
 function startTime_n(){
     let time = moment().tz("Europe/Rome").format("hh:mm:ss");
 
     document.getElementById("clock").textContent = time;
     document.getElementById("clock").innerText = time;
 
-    //setTimeout(startTime_n,1000);
 }
 setInterval(startTime_n, 1000);
